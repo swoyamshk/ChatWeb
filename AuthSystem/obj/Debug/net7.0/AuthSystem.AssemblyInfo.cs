5dc601e425fcbb9cead1bde1818cc433a8b88daf
swoyamr.shakya <swoyamr.shakya@patancollege.edu.np>
2024-08-08 16:33:28 +0545

    added search in chat

--- AuthSystem/obj/Debug/net7.0/AuthSystem.AssemblyInfo.cs
+++ AuthSystem/obj/Debug/net7.0/AuthSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde17bb61ee4c3537a23a3f57bd8b2ed012bfb66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b2aff4e9cbf8efdb0afd8766a71e4d7c78c97c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
